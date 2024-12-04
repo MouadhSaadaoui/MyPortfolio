7a7f4463fc2d11ca9923c7eeb382a44195e53593
MouadhSaadaoui <mouadh.saadaoui@esprit.tn>
2024-12-04 12:32:50 +0100

    commit

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -6,7 +6,7 @@ const QuoteSection = () => {
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
       <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col items-center justify-center">
         <h2 className="text-white text-3xl font-bold text-center mb-4">
-          The best thing about a boolean is even if you are wrong, you are only off by a bit.
+        The best thing about a boolean is even if you are wrong, you are only off by a &quot;bit&quot;.
         </h2>
         <p className="text-[#ADB7BE] text-base text-center">&ndash; Anonymous</p>
       </div>
